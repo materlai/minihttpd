3a35684640374049a94c131ff84c1e3454116e18
masterlai <mater_lai@163.com>
2016-06-23 01:50:02 +0800

    fix listening tcp socket can not reuse addr issue

--- src/tcp_socket.cpp
+++ src/tcp_socket.cpp
@@ -13,19 +13,20 @@ int create_tcp_socket(struct sockaddr *server_addr,uint32_t max_listening_number
 {
 	struct sockaddr_in * addr= (struct sockaddr_in*)server_addr;
 	int socket_fd= socket(addr->sin_family, SOCK_STREAM,0);
+
+    //set reuse socket option for listening server
+	int reuse_addr=1;
+	setsockopt(socket_fd,SOL_SOCKET,SO_REUSEADDR,&reuse_addr,(socklen_t)sizeof(reuse_addr));
+	fd_close_on_exec(socket_fd);
+	
 	socklen_t addr_len=sizeof(*addr);
     if(bind(socket_fd,(struct sockaddr*)addr,addr_len)!=0){
         close(socket_fd);
 		return -1;
 	}
-	//set reuse socket option for listening server
-	int reuse_addr=1;
-	setsockopt(socket_fd,SOL_SOCKET,SO_REUSEADDR,&reuse_addr,(socklen_t)sizeof(reuse_addr));
-	fd_close_on_exec(socket_fd);
 
 	//start to listening
-	listen(socket_fd,max_listening_number);
-	
+	listen(socket_fd,max_listening_number);	
 	return socket_fd;
 }
 
